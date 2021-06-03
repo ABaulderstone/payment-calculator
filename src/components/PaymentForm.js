41d5bc0aa15803da8653a8a64662590c0e532ad1
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-06-03 11:59:35 +1000

    changes useForm to return object

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -3,10 +3,11 @@ import { Input, Button, Label, Form } from './styled-components';
 import useForm from '../custom-hooks/use-form'
 
 const PaymentForm = (props) => {
+    const onFormSubmit = () => console.log(values);
     const defaultValues = {payment:0, income:0}
-    const [values, handleChange] = useForm(defaultValues);
+    const {values, handleSubmit, handleChange} = useForm({defaultValues, onSubmit: onFormSubmit});
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Label htmlFor='income'>Income:</Label>
             <Input id='income' type='number' name="income" value={values.income} onChange={handleChange}/>
             <Label htmlFor='payment'>Payment:</Label>
--- src/custom-hooks/use-form.js
+++ src/custom-hooks/use-form.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
-const useForm = (defaultValues) => {
+const useForm = (options) => {
+    const {defaultValues, onSubmit} = options;
     const [values, setValues] = useState(defaultValues);
     const handleChange = (event) => {
         setValues( values => {
@@ -8,9 +9,16 @@ const useForm = (defaultValues) => {
                 [event.target.name]: event.target.value
             }
         })
+    }; 
+    const handleSubmit = (event) => {
+        // if(event) {
+        //     event.preventDefault()
+        // }
+        event && event.preventDefault();
+        onSubmit();
     }
 
-    return [values, handleChange]
+    return {values, handleChange, handleSubmit}
 };
 
 export default useForm
