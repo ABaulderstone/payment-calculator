7e80a5f5d301d993af4c1bdc4204620f1a351519
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-06-09 13:56:29 +1000

    change validation to regex and text field

--- src/components/PaymentForm/index.js
+++ src/components/PaymentForm/index.js
@@ -13,16 +13,16 @@ const PaymentForm = (props) => {
         setPaymentValue(`$${payment.toFixed(2)}`);
     }
     
-    const defaultValues = {payment:0, income:0}
+    const defaultValues = {payment:"0.00", income:"0.00"}
     const {values, handleSubmit, handleChange, errors} = useForm({defaultValues, onSubmit: onFormSubmit, validate});
     
     return (
         <Form onSubmit={handleSubmit}>
             <Label htmlFor='income'>Income:</Label>
-            <Input error={errors.income} id='income' step="0.1" type='number' name="income" value={values.income} onChange={handleChange}/>
+            <Input error={errors.income} id='income' type='text' name="income" value={values.income} onChange={handleChange}/>
             {errors.income && <p style={{color: "red"}}>{errors.income}</p>}
             <Label htmlFor='payment'>Payment:</Label>
-            <Input error={errors.payment} step="0.1" id='payment' type='number' name="payment" value={values.payment} onChange={handleChange}/>
+            <Input error={errors.payment}  id='payment' type='text' name="payment" value={values.payment} onChange={handleChange}/>
             {errors.payment && <p style={{color: "red"}}>{errors.payment}</p>}
             <Button color="#ffb6c1" type='submit'>Calculate</Button>
         </Form>
--- src/components/PaymentForm/validation-rules.js
+++ src/components/PaymentForm/validation-rules.js
@@ -1,11 +1,22 @@
+const twoDecimalRegex = /^[0-9]+(\.[0-9]{1,2})?$/;
+
 const validate = (values) => {
     let errors = {}; 
     if(values.income === "") {
         errors.income = "Income field cannot be empty"
     }; 
     if(values.payment === "") {
-        errors.payment= "Payment field cannot be empty"
-    }; 
+        errors.payment = "Payment field cannot be empty"
+    };
+    
+    if(twoDecimalRegex.test(values.income) === false) {
+        errors.income = "Income field must be a postive number with up to two decimal places"
+
+    }
+
+    if(twoDecimalRegex.test(values.payment) === false) {
+        errors.payment = "Payment field must be a postive number with up to two decimal places"
+    }
 
     return errors;
 }
